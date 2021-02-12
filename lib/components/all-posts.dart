edc6cf51e66e125c846180594268724212bc1040
Aiarsien Diachkovskii <ayarsen1995@gmail.com>
2021-02-12 19:15:16 +0900

    Добавил предварительно истории и подкорректировал посты

--- lib/components/all-posts.dart
+++ lib/components/all-posts.dart
@@ -1,60 +1,86 @@
-import 'package:flutter/material.dart';
-import 'package:real/domain/order.dart';
-import 'package:waterfall_flow/waterfall_flow.dart';
+import 'dart:math';
 
-class AllPosts extends StatefulWidget {
-  @override
-  _AllPostsState createState() => _AllPostsState();
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+List<IntSize> _createSizes(int count) {
+  Random rnd = new Random();
+  return new List.generate(count,
+      (i) => new IntSize((rnd.nextInt(500) + 200), rnd.nextInt(500) + 100));
 }
 
-class _AllPostsState extends State<AllPosts> {
-  List<Widget> _imageListLeft(int count) => List.generate(
-        count,
-        (i) => FittedBox(
-          alignment: Alignment.topCenter,
-          fit: BoxFit.fill,
-          child: Container(
-            padding: EdgeInsets.all(2),
-            child: Image.asset(
-              'assets/images/posts/left/i$i' + '_200.jpg',
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-      );
-  List<Widget> _imageListRight(int count) => List.generate(
-        count,
-        (i) => FittedBox(
-          alignment: Alignment.topCenter,
-          child: Container(
-            alignment: Alignment.topCenter,
-            padding: EdgeInsets.all(2),
-            child: Image.asset(
-              'assets/images/posts/right/i$i' + '_200.jpg',
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-      );
+class AllPosts extends StatelessWidget {
+  AllPosts() : _sizes = _createSizes(_kItemCount).toList();
+
+  static const int _kItemCount = 1000;
+  final List<IntSize> _sizes;
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Row(children: <Widget>[
-        Expanded(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: _imageListLeft(3),
+    return new Scaffold(
+      body: new StaggeredGridView.countBuilder(
+        primary: false,
+        crossAxisCount: 2,
+        mainAxisSpacing: 1.0,
+        crossAxisSpacing: 1.0,
+        itemBuilder: (context, index) => new _Tile(index, _sizes[index]),
+        staggeredTileBuilder: (index) => new StaggeredTile.fit(1),
+      ),
+    );
+  }
+}
+
+class IntSize {
+  const IntSize(this.width, this.height);
+
+  final int width;
+  final int height;
+}
+
+class _Tile extends StatelessWidget {
+  const _Tile(this.index, this.size);
+
+  final IntSize size;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Card(
+      child: new Column(
+        children: <Widget>[
+          new Stack(
+            children: <Widget>[
+              new Center(
+                child: new FadeInImage.memoryNetwork(
+                  fadeInDuration: Duration(milliseconds: 500),
+                  placeholder: kTransparentImage,
+                  image: 'https://picsum.photos/${size.width}/${size.height}/',
+                ),
+              ),
+            ],
           ),
-        ),
-        Expanded(
-            child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: _imageListRight(3),
-        )),
-      ]),
+          new Padding(
+            padding: const EdgeInsets.all(1.0),
+            child: new Column(
+              children: <Widget>[
+                new Text(
+                  'User number $index',
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                new Text(
+                  'Width: ${size.width}',
+                  style: const TextStyle(color: Colors.grey),
+                ),
+                new Text(
+                  'Height: ${size.height}',
+                  style: const TextStyle(color: Colors.grey),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
